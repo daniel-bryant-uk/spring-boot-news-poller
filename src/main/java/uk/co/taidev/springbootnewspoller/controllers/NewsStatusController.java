bd21b44b6a39e1e81ea79dfa5db4a1f9a9b13464
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2014-03-29 11:07:12 +0000

    Added RestTemplate call to remote spring-boot-news service (could not autowire RestTemplate for some reason?)

--- src/main/java/uk/co/taidev/springbootnewspoller/controllers/NewsStatusController.java
+++ src/main/java/uk/co/taidev/springbootnewspoller/controllers/NewsStatusController.java
@@ -1,17 +1,28 @@
 package uk.co.taidev.springbootnewspoller.controllers;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @EnableAutoConfiguration
 public class NewsStatusController {
 
+    //    @Autowired
+    private RestTemplate restTemplate = new RestTemplate();
+
+
     @RequestMapping("/news")
     @ResponseBody
     String home() {
-        return "No news is good news!";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/news", String.class);
+
+        String data = response.getBody();
+
+        return "Found the following news: " + data;
     }
 }
